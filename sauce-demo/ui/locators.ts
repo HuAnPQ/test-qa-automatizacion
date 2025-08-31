6767efdcc3d300ebbb960018ededdbf92d9615b6
Hugo Ponce <hugoponcequiroz@gmail.com>
2025-08-30 20:44:58 -0500

    Identificadores para Screenplay

---
+++ sauce-demo/ui/locators.ts
@@ -0,0 +1,36 @@
+
+export const LOGIN_PAGE = {
+  USERNAME_INPUT: '[data-test="username"]',
+  PASSWORD_INPUT: '[data-test="password"]',
+  LOGIN_BUTTON: '[data-test="login-button"]',
+  PAGE_HEADER: '[data-test="primary-header"]'
+};
+
+export const PRODUCTS_PAGE = {
+  T_SHIRT: '[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]',
+  JACKET: '[data-test="add-to-cart-sauce-labs-fleece-jacket"]',
+  SHOPPING_CART_LINK: '[data-test="shopping-cart-link"]'
+};
+
+export const CART_PAGE = {
+  PAGE_TITLE: '[data-test="title"]',
+  CHECKOUT_BUTTON: '[data-test="checkout"]'
+};
+
+export const CHECKOUT_INFO_PAGE = {
+  FIRST_NAME_INPUT: '[data-test="firstName"]',
+  LAST_NAME_INPUT: '[data-test="lastName"]',
+  POSTAL_CODE_INPUT: '[data-test="postalCode"]',
+  CONTINUE_BUTTON: '[data-test="continue"]'
+};
+
+export const CHECKOUT_OVERVIEW_PAGE = {
+  PAGE_TITLE: '[data-test="title"]',
+  TOTAL_LABEL: '[data-test="total-label"]',
+  FINISH_BUTTON: '[data-test="finish"]'
+};
+
+export const CHECKOUT_COMPLETE_PAGE = {
+  PONY_EXPRESS_IMAGE: '[data-test="pony-express"]',
+  COMPLETE_HEADER: '[data-test="complete-header"]'
+};
