e3824fb38b9c674c9158c383daeedb81c5f0a9a6
Hugo Ponce <hugoponcequiroz@gmail.com>
2025-08-30 21:03:18 -0500

    Habilidad para el actor

---
+++ sauce-demo/abilities/BrowseTheWeb.ts
@@ -0,0 +1,34 @@
+
+import { Page, expect, Locator } from '@playwright/test';
+
+export class BrowseTheWeb {
+  private constructor(private page: Page) {}
+
+  static using(page: Page) {
+    return new BrowseTheWeb(page);
+  }
+
+  async navigateTo(url: string) {
+    await this.page.goto(url);
+  }
+
+  async click(selector: string) {
+    await this.page.locator(selector).click();
+  }
+
+  async fill(selector: string, value: string) {
+    await this.page.locator(selector).fill(value);
+  }
+
+  async locate(selector: string): Promise<Locator> {
+    return this.page.locator(selector);
+  }
+
+  async assertText(selector: string, expectedText: string) {
+    await expect(this.page.locator(selector)).toContainText(expectedText);
+  }
+
+  async assertVisible(selector: string) {
+    await expect(this.page.locator(selector)).toBeVisible();
+  }
+}
